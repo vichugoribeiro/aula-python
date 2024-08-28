825bf6f9e2ee96ae34ed6c892d7755c3a6f935b3
vichugoribeiro <victoribeiro.alunosenai@gmail.com>
2024-08-28 20:25:26 -0300

    atualização

--- vh.py
+++ vh.py
@@ -1,13 +1,48 @@
-numero = int (input ("Entrada de N"))
+# numero = int (input ("Entrada de N"))
 
-def verificaEntrada (numero):
+# def verificaEntrada (numero):
 
-    if numero > 0:
-        return "tu numero eres positivo"
-    elif numero<0:
-        return "se o numero negativo"
-    else:
-        return "se numero zero"
-verificaEntrada (numero)
-print(verificaEntrada(numero))
+#     if numero > 0:
+#         return "seu numero é positivo !"
+#     elif numero<0:
+#         return "seu numero é negativo!"
+#     else:
+#         return "seu numero é zero!"
+# print(verificaEntrada(numero))
 
+
+
+
+a = 2
+b = 6
+c = 3
+
+operacaoAnd = a == b and c * a == b 
+print (operacaoAnd)
+operacaoOr = a == b or c * a == b 
+print (operacaoOr)
+
+operacaoAnd = a == b and c * a == b
+print (operacaoAnd)
+operacaoOr = a == b or c * a == b
+print(operacaoOr)
+
+operacaoAnd = a ==(c+b) and b == c
+print (operacaoAnd)
+operacaoOr = a ==(c+b) or b == c
+print (operacaoOr)
+
+operacaoAnd = a > 5 and b > 3 
+print (operacaoAnd)
+operacaoOr = a > 5 or b > 3 
+print ( operacaoOr)
+
+operacaoAnd = not b == 6 and 3 * 2 == b
+print (operacaoAnd)
+operacaoOr = not b == 6 or 3 * 2 == b
+print (operacaoOr)
+
+operacaoAnd = c < 5 and not b > 6
+print (operacaoAnd)
+operacaoOr = c < 5 or not b > 6
+print (operacaoOr)
