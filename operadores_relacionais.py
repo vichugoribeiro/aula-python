21d94408b1ed5eb4bc620391236319f361fc9fe3
vichugoribeiro <victoribeiro.alunosenai@gmail.com>
2024-08-28 22:02:13 -0300

    atualizacao do arquivo vh1

---
+++ operadores_relacionais.py
@@ -0,0 +1,21 @@
+a = 5
+b = 3
+## igualdade
+resultado = a == b
+print(resultado)
+
+# Maior que (>)
+resultado = a > b
+print(resultado)
+
+# Menor eu (<)
+resultado = a < b
+print(resultado)
+
+# maior ou igual (>=)
+resultado = a >= b
+print(resultado)
+
+#menor ou igual (=<)
+resultado = a <= b
+print(resultado)
