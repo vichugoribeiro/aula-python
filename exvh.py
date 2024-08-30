65c765f0592d504530d67b691ccbe66884cfa389
vichugoribeiro <victoribeiro.alunosenai@gmail.com>
2024-08-29 21:54:40 -0300

    arquivo vh

---
+++ exvh.py
@@ -0,0 +1,17 @@
+# dados Marcelo
+ListaEsperaaceito = False 
+indicaco = True 
+diaAtual = 29
+idadeMArcelo = 15
+
+
+# Regras
+dataInscricao = 28
+idadeMinima = 14
+idadeMaxima = 17
+ListaEsperaAceito = False 
+
+if not ListaEsperaAceito:
+    print("Marcelo Não esta na lista")
+else:
+    print("Marcelo esta na lista")
---
+++ o_atribuicao.py
@@ -0,0 +1,18 @@
+x = 5
+
+x += 2
+print(x) # resultado 7
+
+x *= 2
+print(x)# resultado 14
+
+x -= 2
+print(x)# resultado 12
+
+x /= 2
+print(x)# resultado 6
+
+x = 12
+x //= 2
+print(x)# resultado 
+
