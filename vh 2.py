38c8721f6b2c8aff1db1c2c77e0cebf97ea6d9a5
vichugoribeiro <victoribeiro.alunosenai@gmail.com>
2024-09-04 21:01:45 -0300

    Refatoração

--- vh 2.py
+++ vh 2.py
@@ -1,31 +1,17 @@
-# Atividade
-# Suba ao GitHub o código da forma que receber
-#  Melhore o código com funções que podem ser reaproveitadas
-#  Melhore a legibilidade do código
-#  Atualize o GitHub com o código refatorado
-# Dicas
-# ● Teste o Código Antes: Verifique se o código esteja funcional antes de iniciar a refatoração.
-# ● Reveja o Código: Analise o código para identificar áreas que precisam de refatoração.
-# ● Teste Após Refatorar: Certifique-se de que o código ainda funciona conforme esperado após a refatoração.
-# ● Vocês tem total autonomia de refatorar o código da forma que quiserem, ajustar, implementar mais funções, etc.
-
-#Verificacao de preço 
-
-produto1 = 100
-produto2 = 200
-produto3 = 300
-
-total = produto1 + produto2 + produto3
-
+#Variaveis
+produtosValor = [100,200,300]
+total = sum(produtosValor)
+print(f'Total antes do desconto: {total}')
 desconto = 0
-if total > 500:
-    desconto = t * 0.1
-
-r = total - desconto
-
-print('Total antes do desconto:', total)
-print('Desconto aplicado:', desconto)
-print('Total com desconto:', r)
+#Desconto 10%
+if total > 500: 
+    desconto = total - (total * 0.1)
+    resultado = total - desconto
+    print(f'Desconto aplicado: {desconto}')
+    print(f'Total com desconto: {resultado}')
+else:
+    diferenca = 500 - total
+    print(f"Faltou {diferenca} para o desconto ser aplicado! Deseja incluir mais algum produto?")
 
 
 
